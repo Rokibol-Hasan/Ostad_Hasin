8f936fd1d2a2d870ec5482920c5b0493c8b1e72c
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2024-08-19 16:21:48 +0600

    validation and middleware

---
+++ app/Http/Controllers/MidCheckController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MidCheckController extends Controller
+{
+    function checkMid1(Request $request){
+        return response("Hello1");
+    }
+    function checkMid2(Request $request){
+        return response("Hello2");
+    }
+
+    function checkSecret(Request $request){
+        return response("User not recognized");
+    }
+}
